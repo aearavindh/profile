14baf298bc562f44e9d0c7a1f755293b70392a2f
aearavindh <aearavindh@gmail.com>
2022-02-18 00:20:27 +0530

    Education component completed

---
+++ src/components/Accordion.js
@@ -0,0 +1,23 @@
+import { Add, Remove } from "@mui/icons-material";
+import React, { useState } from "react";
+import "./Accordion.css";
+
+function Accordion({ body, children }) {
+  const [active, setActive] = useState(false);
+  return (
+    <div accordion className="accordion">
+      <div onClick={() => setActive(!active)} className="accordion__header">
+        <h3>{children}</h3>
+        {active ? <Remove /> : <Add />}
+      </div>
+      <div
+        style={active ? { display: "block" } : { display: "none" }}
+        className="accordion__body"
+      >
+        <p>{body}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Accordion;
--- src/routes/Education.js
+++ src/routes/Education.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Accordion from "../components/Accordion";
 import "./Education.css";
 
 function Education() {
-  return <div className="education">Education</div>;
+
+  return <div className="education">
+    <Accordion body="Devaswom High School, Thirparappu" >High School</Accordion>
+    <Accordion body="Arunachalam Higher Secondary School, Thiruvattar" >Higher Secondary</Accordion>
+    <Accordion body="University College of Engineering, Villupuram" >College</Accordion>
+  </div>;
 }
 
 export default Education;
