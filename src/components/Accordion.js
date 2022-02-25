0d3240f2395f84f951bca6c6dfea1e18cca38cbd
aearavindh <aearavindh@gmail.com>
2022-02-25 17:33:28 +0530

    Transition added to accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -6,15 +6,22 @@ function Accordion({ body, children }) {
   const [active, setActive] = useState(false);
   return (
     <div className="accordion">
-      <div onClick={() => setActive(!active)} className="accordion__header" style={active ? { borderRadius: "10px 10px 0 0" } : {}}>
+      <div
+        onClick={() => setActive(!active)}
+        className="accordion__header"
+      >
         <h3>{children}</h3>
         {active ? <Remove /> : <Add />}
       </div>
       <div
-        style={active ? { display: "block" } : { display: "none" }}
+        style={
+          active
+            ? { visibility: "visible", opacity: 1 }
+            : { visibility: "hidden", opacity: 0, height: "0px" }
+        }
         className="accordion__body"
       >
-        <p>{body}</p>
+        <p style={active ? { opacity: 1 } : { opacity: 0 }}>{body}</p>
       </div>
     </div>
   );
