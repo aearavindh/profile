ec5e2ba6c709a51f7c5ca72edbe24b7f3b0a52ab
aearavindh <aearavindh@gmail.com>
2022-02-28 20:01:25 +0530

    Sorting Visualizer project added

--- src/constants/Projects.js
+++ src/constants/Projects.js
@@ -2,6 +2,7 @@ import TicTacToePreview from "../assets/images/projects/tic-tac-toe.png";
 import LinkedinClonePreview from "../assets/images/projects/linkedin-clone.png";
 import AmazonClonePreview from "../assets/images/projects/amazon-clone.png";
 import FacebookClonePreview from "../assets/images/projects/facebook-clone.png";
+import SortingVisualizerImage from "../assets/images/projects/sorting-visualizer.png";
 
 const projects = [
   {
@@ -23,6 +24,11 @@ const projects = [
     key: "facebookClone",
     title: "Facebook Clone",
     background: FacebookClonePreview,
+  },
+  {
+    key: "sortingVisualizer",
+    title: "Sorting Visualizer",
+    background: SortingVisualizerImage,
   },
 ];
 
